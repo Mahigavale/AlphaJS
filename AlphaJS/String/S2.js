1e3ea858d7ad400dddd9648390f38eaa91f52323
Mahigavale <maheshgavale07@gmail.com>
2025-08-11 12:32:00 +0530

    String methods

---
+++ AlphaJS/String/S2.js
@@ -0,0 +1,18 @@
+let names="udiejfofjopkcpkcpcp";
+//String starts from zero.
+console.log(names.at(-2));
+//from the end.
+console.log(names.at(2));
+//from the start.
+console.log(names.at());
+//zeroth element.
+
+console.log(typeof(names.at(2)));
+
+console.log("***************************************************");
+
+console.log(names.charAt(0));
+console.log(names.charAt(-1));
+console.log((names.charAt(100)));
+
+
---
+++ AlphaJS/String/S3.js
@@ -0,0 +1,19 @@
+let str="ABC";
+
+
+//console.log(str.charCodeAt(0));
+//console.log(str.charCodeAt(1));
+//console.log(str.charCodeAt(-1));
+//console.log(str.charCodeAt());
+
+
+console.log(typeof(str.charCodeAt(1)));
+
+console.log(typeof(str[2]));
+
+//console.log(str[-1]);
+
+
+
+
+
