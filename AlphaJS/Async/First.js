8d6fa5eda2276725fc8fc962868bdf43849cb1a6
Mahigavale <maheshgavale07@gmail.com>
2025-08-25 12:33:27 +0530

    Asnyc and JS

---
+++ AlphaJS/Async/First.js
@@ -0,0 +1,16 @@
+
+let flag=false;
+let timer=setTimeout(print,4000,10,10);
+if(flag==true){
+clearTimeout(timer);
+}
+function print(n,v)
+{
+    flag=true;
+    console.log("hello from the print;"+(n+v));
+    
+}
+console.log("hiii");
+console.log("byeeee");
+
+
